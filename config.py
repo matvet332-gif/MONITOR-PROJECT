2ebc1c4f17716c4ae1d2a1927df7c2a15b5e2c69
matvet332-gif <matvet332@gmail.com>
2026-06-29 23:51:04 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
